a5b338bd52c87e23a8a0643186aa743a22a42fd5
2571964s <2571964s@student.gla.ac.uk>
2022-11-02 18:36:20 +0000

    Works

--- Web-Monitor/tldlist.c
+++ Web-Monitor/tldlist.c
@@ -26,36 +26,6 @@ struct tlditerator {
     struct tldnode *list_root;
 };
 
-#define COUNT 10
-
-void print2DUtil(TLDNode *root, int space) {
-    // Base case
-    if (root == NULL)
-        return;
-
-    // Increase distance between levels
-    space += COUNT;
-
-    // Process right child first
-    print2DUtil(root->right, space);
-
-    // Print current node after space
-    // count
-    printf("\n");
-    for (int i = COUNT; i < space; i++)
-        printf(" ");
-    printf("|%s %d|\n", root->hostname, root->count);
-
-    // Process left child
-    print2DUtil(root->left, space);
-}
-
-// Wrapper over print2DUtil()
-void print2D(TLDNode *root) {
-    // Pass initial space count as 0
-    print2DUtil(root, 0);
-}
-
 TLDList *tldlist_create(Date *begin, Date *end) {
     TLDList *tld = NULL;
     if ((tld = (TLDList *)malloc(sizeof(TLDList)))) {
